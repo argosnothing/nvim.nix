e9bf5cf658bd27aeb3f60f9732a1e1bdb8890aec
salivala <argosnothing@gmail.com>
2026-03-22 16:27:19 -0400

    update to have a better terminal default keybind. ( i don't use tags )

--- nvim/lua/utility/terminal.lua
+++ nvim/lua/utility/terminal.lua
@@ -5,52 +5,52 @@ local terminal_win = nil
 
 -- Hide line numbers in the terminal
 vim.api.nvim_create_autocmd("TermOpen", {
-	callback = function()
-		vim.opt_local.number = false
-		vim.opt_local.relativenumber = false
-	end,
+    callback = function()
+        vim.opt_local.number = false
+        vim.opt_local.relativenumber = false
+    end,
 })
 
 --- Close the terminal window without entering insert mode.
 --- Safe to call even if the terminal is already closed.
 M.close = function()
-	if terminal_win and vim.api.nvim_win_is_valid(terminal_win) then
-		vim.api.nvim_win_close(terminal_win, true)
-		terminal_win = nil
-	end
+    if terminal_win and vim.api.nvim_win_is_valid(terminal_win) then
+        vim.api.nvim_win_close(terminal_win, true)
+        terminal_win = nil
+    end
 end
 
 local function toggle()
-	-- Terminal is visible: hide it
-	if terminal_win and vim.api.nvim_win_is_valid(terminal_win) then
-		vim.api.nvim_win_close(terminal_win, true)
-		terminal_win = nil
-		return
-	end
+    -- Terminal is visible: hide it
+    if terminal_win and vim.api.nvim_win_is_valid(terminal_win) then
+        vim.api.nvim_win_close(terminal_win, true)
+        terminal_win = nil
+        return
+    end
 
-	-- Terminal is hidden: close DAP UI first (if loaded), then open terminal
-	if package.loaded["dap"] then
-		require("utility.dap_config").close_ui()
-	end
+    -- Terminal is hidden: close DAP UI first (if loaded), then open terminal
+    if package.loaded["dap"] then
+        require("utility.dap_config").close_ui()
+    end
 
-	if terminal_buf and vim.api.nvim_buf_is_valid(terminal_buf) then
-		vim.cmd("botright split")
-		vim.cmd("resize 15")
-		terminal_win = vim.api.nvim_get_current_win()
-		vim.api.nvim_win_set_buf(terminal_win, terminal_buf)
-	else
-		vim.cmd("botright split")
-		vim.cmd("resize 15")
-		vim.cmd("terminal")
-		vim.opt_local.buflisted = false
-		terminal_buf = vim.api.nvim_get_current_buf()
-		terminal_win = vim.api.nvim_get_current_win()
-	end
+    if terminal_buf and vim.api.nvim_buf_is_valid(terminal_buf) then
+        vim.cmd("botright split")
+        vim.cmd("resize 15")
+        terminal_win = vim.api.nvim_get_current_win()
+        vim.api.nvim_win_set_buf(terminal_win, terminal_buf)
+    else
+        vim.cmd("botright split")
+        vim.cmd("resize 15")
+        vim.cmd("terminal")
+        vim.opt_local.buflisted = false
+        terminal_buf = vim.api.nvim_get_current_buf()
+        terminal_win = vim.api.nvim_get_current_win()
+    end
 
-	vim.cmd("startinsert")
+    vim.cmd("startinsert")
 end
 
-vim.keymap.set({ "n", "t" }, "<M-t>", toggle, { silent = true, desc = "Terminal: Toggle" })
+vim.keymap.set({ "n", "t" }, "<C-t>", toggle, { silent = true, noremap = true, desc = "Terminal: Toggle" })
 
 -- Escape in terminal mode returns to normal mode
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { silent = true })
