07529408772cc85326bdb6baf73254820e94f206
salivala <argosnothing@gmail.com>
2026-03-22 14:13:53 -0400

    added more stuff

--- nvim/lua/languages/rust.lua
+++ nvim/lua/languages/rust.lua
@@ -1,19 +1,30 @@
 local lsp = require("languages.lsp")
 
 vim.lsp.config("rust_analyzer", {
-    cmd = { "rust-analyzer" },
+    cmd = { "false" },
     filetypes = {},
-    root_markers = { "Cargo.toml", "rust-project.json", ".git" },
-    capabilities = lsp.capabilities,
+    root_markers = {},
 })
+
+local function start_rust_lsp()
+    vim.lsp.start({
+        name = "rust_analyzer",
+        cmd = { "rust-analyzer" },
+        root_dir = vim.fs.root(0, { "Cargo.toml", "rust-project.json" }),
+        capabilities = lsp.capabilities,
+    })
+end
 
 vim.api.nvim_create_autocmd("User", {
     pattern = "DirenvLoaded",
     once = true,
     callback = function()
-        vim.lsp.config("rust_analyzer", {
-            filetypes = { "rust" },
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "rust",
+            callback = start_rust_lsp,
         })
-        vim.lsp.enable("rust_analyzer")
+        if vim.bo.filetype == "rust" then
+            start_rust_lsp()
+        end
     end,
 })
