6d33b245787bf405c9f1844a14d59c5da618cd46
salivala <argosnothing@gmail.com>
2026-03-22 14:01:43 -0400

    fix fox direnv

--- nvim/lua/languages/rust.lua
+++ nvim/lua/languages/rust.lua
@@ -2,7 +2,7 @@ local lsp = require("languages.lsp")
 
 vim.lsp.config("rust_analyzer", {
     cmd = { "rust-analyzer" },
-    filetypes = { "rust" },
+    filetypes = {},
     root_markers = { "Cargo.toml", "rust-project.json", ".git" },
     capabilities = lsp.capabilities,
 })
@@ -11,7 +11,9 @@ vim.api.nvim_create_autocmd("User", {
     pattern = "DirenvLoaded",
     once = true,
     callback = function()
+        vim.lsp.config("rust_analyzer", {
+            filetypes = { "rust" },
+        })
         vim.lsp.enable("rust_analyzer")
-        vim.cmd("doautoall FileType rust")
     end,
 })
